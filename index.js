1f5a608f183132b015db51546572cae5080ac8fd
Syaifuddin1997 <msyaifuddin1997@gmail.com>
2022-06-05 20:40:22 +0700

    commit updated

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
-
+const sorter = (sortData,funcSort) => {
+  let resultList = funcSort(sortData)
+  let sorterData = [];
+  
+  for(let i = 0; i < resultList.length; i++) {
+    sorterData.push(`${i+1}. ${resultList[i]}`);
+  }
+  return sorterData;
+}
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
-
+const sortAscending = (arrSorter) => {
+  return arrSorter.sort()
+} 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrSorter)=> {
+  return arrSorter.sort().reverse();
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
